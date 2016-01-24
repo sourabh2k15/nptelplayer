57f592ea1d4bd599282fe41d24b2708c1a0994a4
sourabh2k15 <sourab.reddy2k14"gmail.com>
2016-01-25 02:42:47 +0530

    added beautiful frontend

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 var http = require('http');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 var port = 5000;
-app.use(express.static(__dirname+'/public/'));
+var dir = 'public/videos/';
 
+app.use(express.static(__dirname+'/public/'));
 var server = http.createServer(app);
+
 app.listen(port,function(req,res){
   console.log("server running on port "+port);
+});
+
+app.get('/subjects',function(req,res){
+    var subjects = [];
+    fs.readdirSync(dir).forEach(function(file){
+        subjects.push(file.toString());
+    });
+    res.send(JSON.stringify(subjects));
+});
+
+app.get('/videos',function(req,res){
+    var subject = (req.query).subject;
+    console.log(subject);
+    var videos = [];
+    fs.readdirSync(dir+'/'+subject+'/videos/nptel/').forEach(function(file){
+        videos.push(file);
+    });
+    res.send(JSON.stringify(videos));
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,64 @@
+var csubject="";
 $(document).ready(function(){
-   console.log("app ready!!");
+    console.log("app ready to use!");
+    getsubjects();
 });
+
+function getsubjects(){
+    console.log('requesting subjects from server');
+    $.ajax({
+        type:'get',
+        url:'/subjects',
+        success:function(data){
+            var subjects = [];
+            subjects = JSON.parse(data);
+            console.log(subjects);
+            var html='<option>Select your subject</option>';
+            for(var i=0;i<subjects.length;i++){
+                html+='<option id="option"+'+i+'>'+subjects[i]+'</option>';
+            }
+            _('subjects').innerHTML = html;
+        },
+        error: function(){
+            console.log("some error occurred!");
+        }
+    });
+}
+
+function getvideos(subject){
+    csubject = subject;
+    console.log(subject);
+    $.ajax({
+        type:'get',
+        url:'/videos',
+        data:{subject:subject},
+        success:function(data){
+            var videos = [];
+            videos = JSON.parse(data);
+            var html='<option>Select a video</option>';
+            
+            for(var i=0;i<videos.length;i++){
+                html+='<option id="video"+'+i+'>'+videos[i]+'</option>';
+            }
+            _('videos').innerHTML = html;
+        },
+        error:function(){
+            console.log('some error occurred!!');
+        }
+    });
+}
+
+function firevideo(video){
+    $('#top').slideUp();
+    $('#videocont').fadeIn();
+    _('player').src = "videos/"+csubject+"/videos/nptel/"+video;
+    _('player').play();
+}
+
+function relax(){
+    console.log("user wants to relax!!");
+}
+
+function _(el){
+    return document.getElementById(el);
+}
